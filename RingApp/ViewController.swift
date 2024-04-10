c1e3224f5577421a51a545048a969d3f2434b9fc
Emre Yeler <yeleremre@hotmail.com>
2024-04-10 21:39:22 +0300

    Initial commit

---
+++ RingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RingApp
+//
+//  Created by Emre on 10.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World") 
+    }
+
+
+}
+
